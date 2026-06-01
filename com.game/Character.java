4767c9b7e85b37d44d9506b9bbc97159fd545436
white <75066048+namuwe@users.noreply.github.com>
2026-06-01 17:23:44 +0900

    Create Character.java

---
+++ com.game/Character.java
@@ -0,0 +1,31 @@
+package com.game;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Character {
+
+	public static class Player {
+		private static final String name = "테스트";
+		private static int x = 5;
+		private static int y = 4;
+		public static final Image PlayerImage = new ImageIcon(Character.class.getResource("/image/player.png"))
+				.getImage();
+		public static int getX() {
+			return x;
+		}
+
+		public static void setX(int x) {
+			Player.x = x;
+		}
+
+		public static int getY() {
+			return y;
+		}
+
+		public static void setY(int y) {
+			Player.y = y;
+		}
+	}
+}
